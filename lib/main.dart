923234555bdb0653d1b84ba667ca40790003a9a5
izerekerie <izereuwonkundakelia@gmail.com>
2025-07-21 18:38:54 +0200

    resolve conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,19 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import 'package:blood_system/blocs/hospital/bloc.dart';
+import 'package:blood_system/screens/appointments/book_appointment.dart';
 import 'package:blood_system/screens/home.dart';
 import 'package:blood_system/screens/hospitalAdminRegister.dart';
 import 'package:blood_system/screens/landing.dart';
 import 'package:blood_system/screens/welcomepage.dart';
 import 'package:blood_system/service/hospital_service.dart';
-=======
-import 'package:blood_system/screens/volunteerRegister.dart';
-import 'package:blood_system/screens/welcomepage.dart';
->>>>>>> c28ded0 (add: routes)
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'firebase_options.dart';
-=======
-import 'package:blood_system/screens/appointments.dart';
-=======
-import 'package:blood_system/screens/appointments/appointments.dart';
-import 'package:blood_system/screens/appointments/book_appointment.dart';
-import 'package:blood_system/screens/appointments_router.dart';
->>>>>>> 2bf3f36 (feat: volunteer appointments)
-=======
-import 'package:blood_system/screens/appointments/book_appointment.dart';
->>>>>>> 85e9384 (fix/: gradle)
-import 'package:blood_system/screens/volunteerRegister.dart';
-import 'package:blood_system/screens/welcomepage.dart';
-// import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-// import 'firebase_options.dart';
->>>>>>> 4156bc3 (feat: appointments screen admin)
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-<<<<<<< HEAD
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
-=======
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
-  runApp(MyApp());
->>>>>>> c28ded0 (add: routes)
 }
 
 class MyApp extends StatelessWidget {
@@ -51,7 +21,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return MultiBlocProvider(
       providers: [
         BlocProvider(
@@ -66,29 +35,9 @@ class MyApp extends StatelessWidget {
           '/landing': (context) => const LandingPage(),
           '/home': (context) => const HomePage(),
           '/hospitalAdminRegister': (context) => const HospitalAdminRegister(),
+          '/appointments': (context) => const BookAppointmentScreen(),
         },
       ),
-=======
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false
-,
-      // home: Placeholder()
-      initialRoute: '/appointments',
-      routes: {
-        '/': (context) => Welcomepage(),
-         '/registration': (context) => VolunteerRegister(),
-<<<<<<< HEAD
-<<<<<<< HEAD
-         '/appointments': (context) => AppointmentsScreen(),
-      },
->>>>>>> c28ded0 (add: routes)
-=======
-        '/appointments': (context) => AppointmentsRouter(),      },
->>>>>>> 2bf3f36 (feat: volunteer appointments)
-=======
-        '/appointments': (context) => BookAppointmentScreen(),      },
->>>>>>> 5a00b63 (feat: book appointment)
     );
   }
 }
